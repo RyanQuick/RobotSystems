7a7f98a76fd6785e7f0159eeb2ea4326243f5c26
Ryan Quick <51896082+RyanQuick@users.noreply.github.com>
2021-04-13 13:30:30 -0700

    Calibration all done, testing move_forward
    All three survos should be calibrated and working. Included is a test function that should make all three move from -40 to 40. Now testing function to make the car drive forward at a designated speed and angle.

--- simple_picarx_commands.py
+++ simple_picarx_commands.py
@@ -16,46 +16,30 @@ except ImportError :
            . Shadowing hardware calls with substitute functions ")
     from sim_ezblock import *
 
-
 import picarx_improved
-
 import time
-
-# import sys
-# sys.path.append(r'/opt/ezblock')
-# from ezblock import __reset_mcu__
-# import time
-# __reset_mcu__()
-# time.sleep(0.01)
-
-# from picarmini import dir_servo_angle_calibration
-# from picarmini import forward
-# from ezblock import delay
-# from picarmini import backward
-# from picarmini import set_dir_servo_angle
-# from picarmini import stop
-
-
-# dir_servo_angle_calibration(0)
 
 def move_forward(direction,speed):
     picarx_improved.set_dir_servo_angle(direction)
-    # picarx_improved.forward(speed)
-    print('direction: ', direction, 'speed: ', speed)
+    picarx_improved.forward(speed)
+    time.sleep(0.5)
+    picarx_improved.stop()
     
 def test():
     picarx_improved.dir_servo_angle_calibration(-10) 
     picarx_improved.set_dir_servo_angle(-40)
-    picarx_improved.time.sleep(1)
-    picarx_improved.set_dir_servo_angle(0)
+    picarx_improved.camera_servo_pin1.angle(-40)
+    picarx_improved.camera_servo_pin2.angle(-40)
     time.sleep(1)
     picarx_improved.set_motor_speed(1, 1)
     picarx_improved.set_motor_speed(2, 1)
-    #camera_servo_pin1.angle(0)
-    #camera_servo_pin2.angle(0)
+    picarx_improved.set_dir_servo_angle(40)
+    picarx_improved.camera_servo_pin1.angle(40)
+    picarx_improved.camera_servo_pin2.angle(40)
+    time.sleep(1)
+
     
 if __name__ == "__main__":
-    picarx_improved.dir_servo_angle_calibration(0)
     while True:
-        #move_forward(i,100)
-        test()
+        #test()
+        move_forward(-40,50)
