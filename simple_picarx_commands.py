72b84baaac19014f2219cea64dcc7f052fa1aaca
Ryan Quick <51896082+RyanQuick@users.noreply.github.com>
2021-04-12 21:43:48 -0700

    More aggressive angle testing

--- simple_picarx_commands.py
+++ simple_picarx_commands.py
@@ -20,12 +20,15 @@ import picarx_improved
 # dir_servo_angle_calibration(0)
 
 def move_forward(direction,speed):
-    picarx_improved.set_dir_servo_angle(direction)
+    # picarx_improved.set_dir_servo_angle(direction)
     # picarx_improved.forward(speed)
-    print("direction: ",direction,"speed: ", speed)
+    # print("direction: ",direction,"speed: ", speed)
+    pass
+    
+    
     
 if __name__ == "__main__":
-    picarx_improved.dir_servo_angle_calibration(-60)
+    picarx_improved.dir_servo_angle_calibration(0)
     while True:
-        move_forward(0,100)
-        
+        #move_forward(0,100)
+        picarx_improved.set_dir_servo_angle(0)
