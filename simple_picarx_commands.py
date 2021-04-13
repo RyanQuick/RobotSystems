fc56d8db3050e7f3eab46d3db4e848ae3fd33efd
Ryan Quick <51896082+RyanQuick@users.noreply.github.com>
2021-04-13 12:34:24 -0700

    Trying to fix print statement issue

--- simple_picarx_commands.py
+++ simple_picarx_commands.py
@@ -8,6 +8,7 @@ from math import tan, pi
 try :
     from ezblock import __reset_mcu__
     from ezblock import *
+    from ezblock import delay
     
     __reset_mcu__()
     time.sleep(0.01)
@@ -36,9 +37,9 @@ except ImportError :
 # dir_servo_angle_calibration(0)
 
 def move_forward(direction,speed):
-    picarx_improved.set_dir_servo_angle(-60+direction)
+    picarx_improved.set_dir_servo_angle(direction)
     # picarx_improved.forward(speed)
-    print("direction: ",-60+direction,"speed: ", speed)
+    print('direction: ', direction, 'speed: ', speed)
     
     pass
     
@@ -46,7 +47,7 @@ def move_forward(direction,speed):
     
 if __name__ == "__main__":
     picarx_improved.dir_servo_angle_calibration(0)
-    i = 0
+    i = -60
     for j in range(12):
         move_forward(i,100)
         i += 10
