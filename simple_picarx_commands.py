9be9fbe1ef0580b469813d23460bdcd45aa4b695
Ryan Quick <51896082+RyanQuick@users.noreply.github.com>
2021-04-13 13:46:23 -0700

    Testing out first iteration of parallel parking

--- simple_picarx_commands.py
+++ simple_picarx_commands.py
@@ -10,7 +10,7 @@ try :
     
     __reset_mcu__()
     time.sleep(0.01)
-except ImportError :
+except ImportError:
     print (" This computer does not appear to be a PiCar -\
            X system(/ opt / ezblock is not present ) \
            . Shadowing hardware calls with substitute functions ")
@@ -19,11 +19,20 @@ except ImportError :
 import picarx_improved
 import time
 
-def move_forward(direction,speed):
-    picarx_improved.set_dir_servo_angle(direction)
+def move_forward(speed,length,angle):
+    picarx_improved.set_dir_servo_angle(angle)
     picarx_improved.forward(speed)
-    time.sleep(0.5)
+    time.sleep(length)
     picarx_improved.stop()
+    
+def pl_park(speed,length, direction=-1):
+    picarx_improved.set_dir_servo_angle(direction*50)
+    picarx_improved.backward(speed)
+    time.sleep(length/2)
+    picarx_improved.set_dir_servo_angle(-direction*50)
+    time.sleep(length/2)
+        
+        
     
 def test():
     picarx_improved.dir_servo_angle_calibration(-10) 
@@ -40,10 +49,11 @@ def test():
 
 
 
-import atexit
-atexit.register(picarx_improved.stop)
+# import atexit
+# atexit.register(picarx_improved.stop)
 
 if __name__ == "__main__":
     # while True:
     # test()
-    move_forward(-40,50)
+    # move_forward(50,1,-40)
+    pl_park(50, 1.5)
