58995af3a27ddc2ff436556c7477b8dbdc7778fd
Ryan Quick <51896082+RyanQuick@users.noreply.github.com>
2021-04-19 13:57:19 -0700

    Looking for "can't connect to x" issue

--- Open_CV_Test.py
+++ Open_CV_Test.py
@@ -6,17 +6,17 @@ def main():
     camera.set(3, 640)
     camera.set(4, 480)
 
-    while( camera.isOpened()):
-        _, image = camera.read()        
-        cv2.imshow('Original', image)
+    # while( camera.isOpened()):
+    #     _, image = camera.read()        
+    #     cv2.imshow('Original', image)
         
-        b_w_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('B/W', b_w_image)
+    #     b_w_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #     cv2.imshow('B/W', b_w_image)
         
-        if cv2.waitKey(1) & 0xFF == ord('q') :
-            break
+    #     if cv2.waitKey(1) & 0xFF == ord('q') :
+    #         break
             
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
     
 if __name__ == '__main__':
     main()
