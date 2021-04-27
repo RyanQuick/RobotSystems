1bd93f4f9b486f49488a3789113ae8224af1926a
Ryan Quick <51896082+RyanQuick@users.noreply.github.com>
2021-04-27 16:11:52 -0700

    Controller_consumer triggered when other simultaneous commands were commented out.

--- picarx_simultaneity.py
+++ picarx_simultaneity.py
@@ -88,9 +88,9 @@ def simultaneity(speed):
     out_bus = DataBus()
     
     
-    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        # bus_sensor = executor.submit(sensor_producer, in_bus, sensor_delay)
-        # bus_interpreter = executor.submit(interpreter_cp, in_bus, out_bus, interpreter_delay)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        bus_sensor = executor.submit(sensor_producer, in_bus, sensor_delay)
+        bus_interpreter = executor.submit(interpreter_cp, in_bus, out_bus, interpreter_delay)
         bus_controller = executor.submit(controller_consumer, out_bus, controller_delay, speed)
         logging.info("Continuing bus grayscale line chasing")
 
