eb5919bc4ec509ced23884780e01dac5cc6f67b9
Ryan Quick <51896082+RyanQuick@users.noreply.github.com>
2021-04-27 14:57:55 -0700

    Referencing wrong function in bus functionality
    @ with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:

--- picarx_simultaneity.py
+++ picarx_simultaneity.py
@@ -91,7 +91,7 @@ def simultaneity(speed):
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         bus_sensor = executor.submit(sensor_producer, in_bus, sensor_delay)
         bus_interpreter = executor.submit(interpreter_cp, in_bus, out_bus, interpreter_delay)
-        bus_controller = executor.submit(controller_consumer, in_bus, controller_delay, speed)
+        bus_controller = executor.submit(controller_consumer, out_bus, controller_delay, speed)
             
             
         
