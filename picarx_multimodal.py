0cf107078e6b9d7a5bb0d9f656cbea7573830721
Ryan Quick <51896082+RyanQuick@users.noreply.github.com>
2021-05-07 15:44:53 -0700

    Revert "It worked! Now looking to add us implementation"
    This reverts commit fdbde8c171f8864989952ae71235e117b8675e23.

--- picarx_multimodal.py
+++ picarx_multimodal.py
@@ -35,19 +35,20 @@ def multimodal(speed):
     grayin_bus = Bus()
     grayout_bus = Bus()
     
-    ultras_bus = Bus()
+    ultrasin_bus = Bus()
+    ultrasout_bus = Bus()
     
     stopsign = Bus(False)
     
     grayscale_producer = Producer(grayin_bus.set_message(s.get_adc_value(),adc_busvar), grayin_bus, delay = sensor_delay, termination_busses = stopsign, name = "grayscale_producer")
-    # logging.info("Bus Value: {0}".format(i.get_grayscale_value(grayin_bus.get_message(adc_busvar))))
+    logging.info("Bus Value: {0}".format(i.get_grayscale_value(grayin_bus.get_message(adc_busvar))))
     grayscale_cp = ConsumerProducer(grayout_bus.set_message(i.get_grayscale_value(grayin_bus.get_message(adc_busvar)),relative_pos), grayin_bus, grayout_bus, delay = interpreter_delay, termination_busses = stopsign, name = "grayscale_cp")
-    # logging.info("Bus Value: {0}".format(grayout_bus.get_message(relative_pos)))
+    logging.info("Bus Value: {0}".format(grayout_bus.get_message(relative_pos)))
     grayscale_consumer = Consumer(c.line_following(m, grayout_bus.get_message(relative_pos)[0], speed), grayout_bus, delay = interpreter_delay, termination_busses = stopsign, name = "grayscale_consumer")
     
-    ultras_producer = Producer(ultras_bus.set_message(s.get_distance(),ultras_busvar),ultras_bus, delay = sensor_delay, name = "ultras_producer")
-    ultras_consumer = Consumer(c.wall_checking(m, ultras_bus.get_message(ultras_busvar)),delay = controller_delay, name = "ultras_cp")
-    # logging.info("made it here")
-    runConcurrently([grayscale_producer, grayscale_cp, grayscale_consumer, ultras_producer, ultras_consumer])
+    #ultras_producer = Producer(s.get_distance(), delay = sensor_delay, name = "ultras_producer")
+    #ultras_consumer = Consumer(c.wall_checking(m, ultrasout_bus.get_message()),delay = interpreter_delay, name = "ultras_cp")
+    logging.info("made it here")
+    runConcurrently([grayscale_producer, grayscale_cp, grayscale_consumer])#, ultras_producer, ultras_consumer])
     
     
