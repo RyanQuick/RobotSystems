4fc1882f9dd90718e2ab782fa8673d66e54eaddd
Ryan Quick <51896082+RyanQuick@users.noreply.github.com>
2021-05-06 21:24:47 -0700

    Trying a different naming scheme

--- picarx_multimodal.py
+++ picarx_multimodal.py
@@ -38,9 +38,9 @@ def multimodal(speed):
     ultrasin_bus = Bus()
     ultrasout_bus = Bus()
     
-    grayscale_producer = Producer(grayin_bus.set_message(s.get_adc_value(),"grayscale_producer"), grayin_bus, delay = sensor_delay, name = "grayscale_producer")
-    grayscale_cp = ConsumerProducer(grayout_bus.set_message(i.get_grayscale_value(grayin_bus.get_message("grayscale_cp")),"grayscale_cp"), grayin_bus, grayout_bus, delay = interpreter_delay, name = "grayscale_cp")
-    grayscale_consumer = Consumer(c.line_following(m, grayout_bus.get_message("grayscale_consumer"), speed),grayout_bus, delay = interpreter_delay, name = "grayscale_consumer")
+    grayscale_producer = Producer(grayin_bus.set_message(s.get_adc_value(),"grayscale_in"), grayin_bus, delay = sensor_delay, name = "grayscale_producer")
+    grayscale_cp = ConsumerProducer(grayout_bus.set_message(i.get_grayscale_value(grayin_bus.get_message("grayscale_in")),"grayscale_out"), grayin_bus, grayout_bus, delay = interpreter_delay, name = "grayscale_cp")
+    grayscale_consumer = Consumer(c.line_following(m, grayout_bus.get_message("grayscale_out"), speed), grayout_bus, delay = interpreter_delay, name = "grayscale_consumer")
     
     #ultras_producer = Producer(s.get_distance(), delay = sensor_delay, name = "ultras_producer")
     #ultras_consumer = Consumer(c.wall_checking(m, ultrasout_bus.get_message()),delay = interpreter_delay, name = "ultras_cp")
